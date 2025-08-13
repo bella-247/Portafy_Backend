f51525a9ed351903c4e6740df653f41a73b59750
abelmekonen <abelmekonen247@gmail.com>
2025-08-13 19:47:20 +0300

    feat: Update production settings and add render configuration for deployment

--- Backend/Portafy/config/settings/prod.py
+++ Backend/Portafy/config/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 import dj_database_url
 
 DEBUG = False
-ALLOWED_HOSTS = ['yourdomain.com']
+ALLOWED_HOSTS = ['portafy-backend.onrender.com']
 
 DATABASES = {
     "default": dj_database_url.config()
