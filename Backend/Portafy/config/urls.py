cd0a3dcacff5d516b2a075382fbd7903524a9e06
abelmekonen <abelmekonen247@gmail.com>
2025-08-16 15:14:11 +0300

    fix: Update URL patterns to conditionally include debug toolbar and improve formatting

--- Backend/Portafy/config/urls.py
+++ Backend/Portafy/config/urls.py
@@ -15,28 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from pprint import pprint
-
-import debug_toolbar
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 from config import settings
 
-pprint(debug_toolbar.urls)
-
 app_name = "config"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls"), name = "accounts"),  # Include URLs from the accounts app
-    path("pdfs/", include("pdfs.urls"), name = "pdfs"),  # Include URLs from the PDFs app
-    path("websites/", include("websites.urls"), name = "websites"),  # Include URLs from the websites app
-    path("payments/", include("payments.urls"), name = "payments"),  # Include URLs from the payments app
-    path("__debug__/", include(debug_toolbar.urls)),
+    path(
+        "accounts/", include("accounts.urls"), name="accounts"
+    ),  # Include URLs from the accounts app
+    path("pdfs/", include("pdfs.urls"), name="pdfs"),  # Include URLs from the PDFs app
+    path(
+        "websites/", include("websites.urls"), name="websites"
+    ),  # Include URLs from the websites app
+    path(
+        "payments/", include("payments.urls"), name="payments"
+    ),  # Include URLs from the payments app
 ]
 
-# for serving the uploaded files in the media folder
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
+    
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
